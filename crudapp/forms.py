01248051a6ba32ca2b9f3c23120c471b78918f4b
el kachabi <badreddinelkachabi@gmail.com>
2022-04-10 15:22:15 +0200

    first commit

---
+++ crudapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': "Enter Task"
+    }))
+
+    class Meta:
+        model = Task
+        fields = '__all__'
